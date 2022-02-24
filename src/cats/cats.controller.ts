850b68c674e90de90aed7604325398e7e9519f4a
FIvicproFico <fivic@profico.hr>
2022-02-24 15:28:34 +0100

    Implemented all HTTP methods in cats controller

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,28 +1,17 @@
 import {
+  Body,
   Controller,
+  Delete,
   Get,
-  Header,
-  HttpCode,
   Param,
   Post,
-  Redirect,
+  Put,
 } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { UpdateCatDto } from './dto/update-cat.dto';
 
 @Controller('cats')
 export class CatsController {
-  @Post()
-  @HttpCode(204)
-  @Header('Cache-Control', 'none')
-  create(): string {
-    return 'This action adds a new cat';
-  }
-
-  // @Get()
-  // @Redirect('http://localhost:3000', 301)
-  // findAll(): string {
-  //   return 'This action returns all cats';
-  // }
-
   @Get()
   async findAll(): Promise<string> {
     return 'This action returns all cats but async';
@@ -32,5 +21,22 @@ export class CatsController {
   findOne(@Param() params): string {
     console.log(params);
     return `This action returns a #${params.id} cat`;
+  }
+
+  @Post()
+  create(@Body() createCatDto: CreateCatDto): string {
+    console.log(createCatDto);
+    return 'This action adds a new cat';
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto): string {
+    console.log(updateCatDto);
+    return `This action updates name of #${id} cat to ${updateCatDto.name}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): string {
+    return `This action removes a #${id} cat`;
   }
 }
